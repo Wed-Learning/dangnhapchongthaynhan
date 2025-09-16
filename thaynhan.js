f5ef4453a676364bb4aee7c4b17cd4b681a4b59f
Wed-Learning <duy123428250109@gmail.com>
2025-09-16 14:22:23 +0700

    Add files via upload

---
+++ thaynhan.js
@@ -0,0 +1,23 @@
+const correctUsername = "admin";
+const correctPassword = "12345";
+
+function checkLogin() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  if (username === correctUsername && password === correctPassword) {
+  window.location.href = "tc.html"
+} else {
+    alert("Bạn nhập sai! Admin đang quan sát bạn 👀");
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) {
+    e.preventDefault();
+  }
+});
+
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+
+});
